964974f8a4f16d6d8c6e10469b67d24967ad54d0
Your Name <nazik_111@mail.ru>
2018-10-29 01:03:47 +0500

    add table component

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,45 @@ import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Components/home/index';
 import SignIn from './Components/signin';
+import Dashboard from './Components/admin/Dashboard';
+import PrivateRoute from './Components/authRoutes/privateRoutes';
+import PublicRoute from './Components/authRoutes/publicRoutes';
+import AdminMatches from './Components/admin/matches';
 
 const Routes = props => {
+  console.log(props);
   return (
     <Layout>
       <Switch>
-        <Route exact component={SignIn} path="/sign_in" />
-        <Route exact component={Home} path="/" />
+        <PrivateRoute
+          {...props}
+          restricted={true}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
+
+        <PrivateRoute
+          {...props}
+          restricted={true}
+          path="/admin_matches"
+          exact
+          component={AdminMatches}
+        />
+        <PublicRoute
+          {...props}
+          restricted={false}
+          path="/"
+          exact
+          component={Home}
+        />
+        <PublicRoute
+          {...props}
+          restricted={false}
+          path="/sign_in"
+          exact
+          component={SignIn}
+        />
       </Switch>
     </Layout>
   );
